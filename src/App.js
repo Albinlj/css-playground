50bddf0c30bff324e67a9febd1e44c8a190204da
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2020-09-16 07:59:21 +0200

    FirstGame

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import First from "./games/200916-first"
+
+function App() {
+  return (
+    <div className="App">
+      <First/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/games/200916-first/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export const First = (props) => {
+  return (
+    <div>
+      <p>Hejz</p>
+    </div>
+  );
+};
+
+export default First;
